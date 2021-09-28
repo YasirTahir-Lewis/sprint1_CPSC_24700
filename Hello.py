d6442909b06ddf1f9f3f164322d79911632cffee
Yasir Tahir <20649054+YasirTahir-Lewis@users.noreply.github.com>
2021-09-28 00:17:30 -0500

    Delete Hello.py

--- Hello.py
+++
@@ -1,8 +0,0 @@
-# Yasir Tahir  
-# Course: Web and Distributed Programming
-
-firstname = "Yasir"
-lastname = "Tahir's"
-message = "'" + firstname + lastname + " Hello world" + "'"
-print(message)
-
