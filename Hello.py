7dc3d2b9e27023ad85220974d2a919b3d398ead3
Yasir Tahir <yasirtahir@lewisu.edu>
2021-09-28 00:07:34 -0500

    First push uploading all files.

---
+++ Hello.py
@@ -0,0 +1,8 @@
+# Yasir Tahir  
+# Course: Web and Distributed Programming
+
+firstname = "Yasir"
+lastname = "Tahir's"
+message = "'" + firstname + lastname + " Hello world" + "'"
+print(message)
+
